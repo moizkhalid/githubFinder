6a9c413ad3256bfb91b5c45ff4d990cb9a0f970e
moizkhalidtogglemarket <moizkhalid72@gmail.com>
2022-02-20 21:50:36 +0500

    change the home page

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
-import Search from '../users/Search';
-import Users from '../users/Users';
+import React, { Fragment } from "react";
+import Search from "../users/Search";
+import Users from "../users/Users";
 
 const Home = () => (
-  <Fragment>
+  <>
     <Search />
     <Users />
-  </Fragment>
+  </>
 );
 
 export default Home;
